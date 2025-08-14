157bc8340b01bc3db76816421b916fdb0904ce51
SushyDev <xsushy@hotmail.com>
2025-08-14 11:11:13 +0200

    improve vue_ls

--- lua/plugins/lsp/config/vtsls.lua
+++ lua/plugins/lsp/config/vtsls.lua
@@ -1,16 +1,21 @@
 vim.lsp.config("vtsls", {
 	filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
 	settings = {
-		vtsls = { tsserver = { globalPlugins = {} } },
+		vtsls = {
+			tsserver = {
+				globalPlugins = {},
+			},
+		},
 	},
 	before_init = function(_, config)
-		table.insert(config.settings.vtsls.tsserver.globalPlugins, {
-			name = "@vue/typescript-plugin",
-			location =
-			"$HOME/.local/lib/node_modules/@vue/language-server",
-			languages = { "vue" },
-			configNamespace = "typescript",
-			enableForWorkspaceTypeScriptVersions = true,
-		})
+		if require('mason-registry').is_installed('vue-language-server') then
+			table.insert(config.settings.vtsls.tsserver.globalPlugins, {
+				name = "@vue/typescript-plugin",
+				location = vim.fn.stdpath('data') .. "/mason/packages/vue-language-server/node_modules/@vue/language-server",
+				languages = { "vue" },
+				configNamespace = "typescript",
+				enableForWorkspaceTypeScriptVersions = true,
+			})
+		end
 	end,
 })
